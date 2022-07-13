325b35a987a8e1bc4bca9477e01d059f696f0456
Lucien Valstar <ldjvalstar@gmail.com>
2022-07-13 11:22:55 +0200

    Added http controller and DI

--- go.mod
+++ go.mod
@@ -1,6 +1,17 @@
 module gochess
 
-require domain v1.0.0
+require go.uber.org/fx v1.17.1
+require adapter v1.0.0
+
+require (
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/dig v1.14.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
+)
+
 replace domain v1.0.0 => ./domain
+replace adapter v1.0.0 => ./adapter
 
 go 1.18
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"domain"
-	"io/ioutil"
-	"os"
+	"context"
+	"net/http"
+
+	"go.uber.org/fx"
+
+	"adapter"
 )
 
+func main() {
+	fx.New(
+		fx.Provide(http.NewServeMux),
+		fx.Invoke(adapter.New),
+		fx.Invoke(registerHooks),
+	).Run()
+}
+
+func registerHooks(
+	lifecycle fx.Lifecycle, mux *http.ServeMux,
+) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				go http.ListenAndServe(":8080", mux)
+				return nil
+			},
+		},
+	)
+}
+
+/*
 func main() {
 
 	domain.Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
@@ -14,4 +39,4 @@ func main() {
 	chessPlayer1 := domain.ChessAIPlayer{domain.WHITE, "Terminator"}
 	chessPlayer2 := domain.ChessAIPlayer{domain.BLACK, "C3P0"}
 	chessGame.StartGame(chessPlayer1, chessPlayer2)
-}
+}*/
